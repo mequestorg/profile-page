aa3ba62fdca6127c632d1153934b5a47570d1403
Itao-o-bong✨ <45539206+itaobong@users.noreply.github.com>
2024-03-29 14:28:03 +0100

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
-# AMI: Use Amazon Linux
+# Script Name: setup_web_server.sh
+# Description: This script automates the setup of a basic web server environment with Apache HTTP Server.
+# AMI: Use Amazon Linux  
+
+# The script is intended to be used on an Amazon EC2 instance running Amazon Linux, 
+
+# Update system packages
 sudo yum update -y
+# The '-y' flag automatically answers yes to any prompts during the update process.
+
+# Install Apache HTTP Server
 sudo yum install -y httpd
+
+# Start Apache HTTP Server
 sudo systemctl start httpd
+
+# Enable Apache HTTP Server to start on boot
 sudo systemctl enable httpd
 
-# Download and unzip your HTML app
+# Download and unzip HTML app
+# Download the HTML app ZIP file
 wget https://mqbootcamp.blob.core.windows.net/web/profile-main.zip
-unzip profile-main.zip
+# Use wget to download the ZIP file from the specified URL.
 
-# Move your app to the web server's document root
+# Unzip the downloaded ZIP file
+unzip profile-main.zip
+# Unzip the downloaded ZIP file to extract its contents.
+
+# Move HTML app to the web server's document root
+# Move the contents of the extracted directory to the document root of Apache
 sudo mv profile-main/* /var/www/html/
+# Move all files and directories inside the 'profile-main' directory to '/var/www/html/',
+# which is the default document root for Apache HTTP Server.
